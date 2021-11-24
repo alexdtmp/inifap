a080d986221a5f4165eb1777d38b5a8161ef04b1
German2000 <38192814@uaz.edu.mx>
2021-11-24 16:24:20 -0600

    Urls de la app usuarios.
    Se agregaron las urls de la app usuarios a las urls generales del sistema.

--- inifap/urls.py
+++ inifap/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('publicaciones.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
