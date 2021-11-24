07304d15c9d53fe06226f760b902a3cfb2a407b8
German2000 <38192814@uaz.edu.mx>
2021-11-24 16:09:12 -0600

    Update views.py
    Se añadieron las vistas de la lista de usuario y de crear un usuario.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls.base import reverse
+from django.contrib.messages.views import SuccessMessageMixin
+from .models import Usuario
+from .forms import UsuarioForm
+
 
 # Create your views here.
+class UsuarioList(ListView):
+
+    model = Usuario
+
+
+class UsuarioNuevo(SuccessMessageMixin, CreateView):
+
+    model = Usuario
+    form_class = UsuarioForm
+    success_message = "Usuario creado correctamente"
+
+    def get_success_url(self):
+
+        return reverse('usuarios:lista')
